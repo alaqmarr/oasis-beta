0324a0b1f5c5040d88105b0970820923355d9304
Al Aqmar <alaqmarak0810@gmail.com>
2025-11-25 18:43:35 +0530

    .

--- src/types/styled.d.ts
+++ src/types/styled.d.ts
@@ -2,8 +2,43 @@ import 'styled-components';
 
 declare module 'styled-components' {
   export interface DefaultTheme {
-    primary: string;
-    secondary: string;
-    background: string;
+    colors: {
+      transparent: string;
+      text: string;
+      textLight: string;
+      primary: string;
+      primaryDark: string;
+      primaryLight: string;
+      secondary: string;
+      success: string;
+      error: string;
+      transparent80: string;
+      background: {
+        light: string;
+        dark: string;
+        neutral: string;
+      };
+      glass: string;
+      glassBorder: string;
+      shadow: {
+        sm: string;
+        md: string;
+        lg: string;
+        xl: string;
+        glass: string;
+      };
+      theme: {
+        lightMode: {
+          primary: string;
+          secondary: string;
+          background: string;
+        };
+        darkMode: {
+          primary: string;
+          secondary: string;
+          background: string;
+        };
+      };
+    };
   }
 }
